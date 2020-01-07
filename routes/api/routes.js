0428d5ff2efd3ad8eabe1e61312e8a4b902adffc
Gongo <jvilla1714@gmail.com>
2020-01-07 11:27:21 -0500

    Removed redundant code

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -1,8 +1,6 @@
-const https = require('https');
 const stockpriceController = require('../../controllers/stockpriceController');
 const logoController = require('../../controllers/logoController');
 const newsController = require('../../controllers/newsController');
-const fs = require('fs');
 
 const urlPrice = 'https://sandbox.iexapis.com/stable/stock/twtr/quote/latestPrice?token=Tsk_09b57d3b11144c5089408fcd94074fc0'
 const urlLogo = 'https://sandbox.iexapis.com/stable/stock/aapl/logo?token=Tsk_09b57d3b11144c5089408fcd94074fc0'
